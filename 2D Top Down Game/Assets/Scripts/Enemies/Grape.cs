d5631e76b84ad50b6203ed5368ee70383f4bfcac
hitechyeti <hitechyeti@gmail.com>
2024-02-26 14:03:24 -0500

    mid grape lob attack

---
+++ 2D Top Down Game/Assets/Scripts/Enemies/Grape.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grape : MonoBehaviour, IEnemy
+{
+    [SerializeField] private GameObject grapeProjectilePrefab;
+
+    private Animator anim;
+    private SpriteRenderer spriteRenderer;
+
+    readonly int ATTACK_HASH = Animator.StringToHash("Attack");
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    public void Attack()
+    {
+        anim.SetTrigger(ATTACK_HASH);
+
+        if (transform.position.x - PlayerController.Instance.transform.position.x < 0)
+        {
+            spriteRenderer.flipX = false;
+        }
+        else
+        {
+            spriteRenderer.flipX = true;
+        }
+    }
+
+    public void SpawnProjectileAnimEvent()
+    {
+        Instantiate(grapeProjectilePrefab, transform.position, Quaternion.identity);
+    }
+}
---
+++ 2D Top Down Game/Assets/Scripts/Enemies/GrapeProjectile.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrapeProjectile : MonoBehaviour
+{
+    [SerializeField]
+
+    void Start()
+    {
+        
+    }
+
+    private IEnumerator
+}
